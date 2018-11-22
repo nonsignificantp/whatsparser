ad15ec4804cd681b8c87718787f69cc452188615
Agustin <nonsignificantp@gmail.com>
2018-11-22 18:12:56 -0300

    versioning and rewrites description

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='WhatsParser',
-    version='0.6.0',
+    version='0.7.0',
     author='Agustin Rodriguez',
     author_email='agustin.dev@protonmail.com',
-    description='A parser for whatsapp .txt files',
+    description='A parsing tool for whatsapp .txt files',
     long_description=long_description[56:],
     long_description_content_type='text/markdown',
     url='https://github.com/nonsignificantp/whatsparser',
